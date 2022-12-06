6932d80019f827eefc4818625b81571ab87103a8
Abdelrhman Haider <abd097@hotmail.com>
2022-12-06 16:14:21 +0200

    Add String with Var

--- ch_1/code.rb
+++ ch_1/code.rb
@@ -1,1 +1,45 @@
+####### Var
+name = "Haider"
+puts name
+
+
+a = 2
+b = 5
+c = a + b
+p c
+
+# Naming Convention
+#
+# total_price = 10 (Accept)
+# total price = 10 (Not Accept)
+#
+# total_price (Accept)
+# total!price = 10 (Not Accept)
+#
+#
+# total3 = 10 (Accept)
+# 3total = 10 (Not Accept)
+#
+# _total = 10 (Accept)
+# x3 = 10 (Accept)
+# 10text = "" (Not Accept)
+# !total = 10 (Not Accept)
+
+
+# Constants
+# Total = 10
+# Total = Total + 10 (Accept but will see some warning)
+# (irb):3: warning: already initialized constant Total
+# (irb):1: warning: previous definition of Total was here
+
+
+####### String
+
+# difference between single and double quotes
+# >> time_now = Time.new # Get the current time into a variable
+# => Fri Jan 15 16:43:31 +0530 2010
+# >> "Hello world, the time is now #{time_now}"
+# => "Hello world, the time is now Fri Jan 15 16:43:31 +0530 2010"
+# >> 'Hello world, the time is now #{time_now}'
+# => "Hello world, the time is now \#{time_now}"
 
